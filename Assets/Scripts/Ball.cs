07b5d259763af0132c6b58f84ff26c92ca1d132e
Dream Games Enterprise <dreamgames@dreamenterprise.info>
2024-04-06 14:48:28 +0100

    Fixed to Normal

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,84 +4,40 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    PlayerMovement playerMovement;
     Rigidbody2D rb;
     LineRenderer lr;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     float power = 0.001f;
-=======
-    float power = 0.005f;
->>>>>>> parent of 20c7fab (Ball & Paddle Working, Just Need to Fix to Frame-Rate for Consistency)
-=======
-    float power = 0.01f;
->>>>>>> parent of cc8c119 (ignore)
     float maxLength = 1f;
 
     Vector3 dragStartPos;
     bool isDragging = false;
-<<<<<<< HEAD
-<<<<<<< HEAD
     bool canAim = true;
     float aimDelay = 0.2f;
-=======
->>>>>>> parent of cc8c119 (ignore)
     public bool ballIsStationary;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         lr = GetComponent<LineRenderer>();
+        playerMovement = FindObjectOfType<PlayerMovement>();
     }
 
     void Start()
     {
         ballIsStationary = true;
-<<<<<<< HEAD
-        isDragging = false;
-=======
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        lr = GetComponent<LineRenderer>();
->>>>>>> parent of 20c7fab (Ball & Paddle Working, Just Need to Fix to Frame-Rate for Consistency)
-=======
->>>>>>> parent of cc8c119 (ignore)
         lr.positionCount = 0;
+        StartCoroutine(EnableAimingDelay());
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     void FixedUpdate()
-=======
-    /*void FixedUpdate()
->>>>>>> parent of cc8c119 (ignore)
     {
-        if (ballIsStationary)
+        if (ballIsStationary && canAim)
         {
-            ProcessInput()
-        }   
-    }*/
-
-    void Update()
-    {
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            ProcessInput(touch.position, touch.phase);
-        }
-        else if (Input.GetMouseButtonDown(0))
-        {
-            ProcessInput(Input.mousePosition, TouchPhase.Began);
-        }
-        else if (Input.GetMouseButton(0))
-        {
-            ProcessInput(Input.mousePosition, TouchPhase.Moved);
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            ProcessInput(Input.mousePosition, TouchPhase.Ended);
+            rb.velocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+            rb.MovePosition(rb.position + (Vector2)transform.right * power * Time.deltaTime);
         }
     }
 
@@ -94,92 +50,40 @@ public class Ball : MonoBehaviour
             ContactPoint2D contact = collision.contacts[0];
             transform.position = contact.point;
             ballIsStationary = true;
-<<<<<<< HEAD
             StartCoroutine(EnableAimingDelay());
-=======
+        }
+    }
+
+    IEnumerator EnableAimingDelay()
+    {
+        yield return new WaitForSeconds(aimDelay);
+        canAim = true;
+    }
+
     void Update()
     {
-        if (Input.touchCount > 0)
+        if (ballIsStationary && canAim)
         {
-            Touch touch = Input.GetTouch(0);
-            ProcessInput(touch.position, touch.phase);
+            ProcessInput();
         }
-        else if (Input.GetMouseButtonDown(0))
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-            ProcessInput(Input.mousePosition, TouchPhase.Began);
+            DragStart(Input.mousePosition);
         }
         else if (Input.GetMouseButton(0))
         {
-            ProcessInput(Input.mousePosition, TouchPhase.Moved);
+            Dragging(Input.mousePosition);
         }
         else if (Input.GetMouseButtonUp(0))
         {
-            ProcessInput(Input.mousePosition, TouchPhase.Ended);
+            DragRelease(Input.mousePosition);
         }
     }
 
-    void ProcessInput(Vector3 screenPos, TouchPhase phase)
-    {
-        switch (phase)
-        {
-            case TouchPhase.Began:
-                DragStart(screenPos);
-                break;
-            case TouchPhase.Moved:
-                if (isDragging)
-                {
-                    Dragging(screenPos);
-                }
-                break;
-            case TouchPhase.Ended:
-                if (isDragging)
-                {
-                    DragRelease(screenPos);
-                }
-                break;
->>>>>>> parent of 20c7fab (Ball & Paddle Working, Just Need to Fix to Frame-Rate for Consistency)
-        }
-    }
-
-<<<<<<< HEAD
-    IEnumerator EnableAimingDelay()
-=======
-        }    
-    }
-
-    void ProcessInput(Vector3 screenPos, TouchPhase phase)
-    {
-        if (ballIsStationary)
-        {
-            switch (phase)
-            {
-                case TouchPhase.Began:
-                    DragStart(screenPos);
-                    break;
-                case TouchPhase.Moved:
-                    if (isDragging)
-                    {
-                        Dragging(screenPos);
-                    }
-                    break;
-                case TouchPhase.Ended:
-                    if (isDragging)
-                    {
-                        DragRelease(screenPos);
-                    }
-                    break;
-            }
-        }
-    }
-
-    public bool IsBallMoving()
->>>>>>> parent of cc8c119 (ignore)
-    {
-        return rb.velocity.magnitude > 0.1f;
-    }
-
-=======
->>>>>>> parent of 3c05da4 (Merge pull request #1 from Dream-Games-Enterprise/Ball-&-Paddle-Link)
     void DragStart(Vector3 screenPos)
     {
         isDragging = true;
@@ -196,12 +100,10 @@ public class Ball : MonoBehaviour
         Vector3 direction = (draggingPos - dragStartPos).normalized;
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
         angle = Mathf.Clamp(angle, 10f, 170f);
-
         direction = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.right;
 
-        draggingPos = dragStartPos + direction * maxLength/* * Time.deltaTime*/;
+        draggingPos = dragStartPos + direction * maxLength;
 
         lr.positionCount = 2;
         lr.SetPosition(0, dragStartPos);
@@ -217,155 +119,9 @@ public class Ball : MonoBehaviour
 
         Vector3 direction = (dragReleasePos - dragStartPos).normalized;
 
-<<<<<<< HEAD
         rb.AddForce(direction * power, ForceMode2D.Impulse);
-<<<<<<< HEAD
         ballIsStationary = false;
         canAim = false;
         playerMovement.EnableMovement();
-=======
->>>>>>> parent of 20c7fab (Ball & Paddle Working, Just Need to Fix to Frame-Rate for Consistency)
-=======
-        rb.AddForce(direction * power /** Time.deltaTime*/, ForceMode2D.Impulse);
-        ballIsStationary = false;
->>>>>>> parent of cc8c119 (ignore)
     }
 }
-
-    /*Rigidbody2D rb;
-    LineRenderer lr;
-    bool isBallStationary;
-    bool isAiming;
-    Vector2 aimDirection;
-    float launchSpeed = 10f;
-
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        lr = GetComponent<LineRenderer>();
-    }
-
-    void Start()
-    {
-        lr.positionCount = 2;
-        lr.enabled = false;
-        isBallStationary = true;
-        isAiming = false;
-    }
-
-    void Update()
-    {
-        if (isBallStationary)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                isAiming = true;
-                aimDirection = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
-            }
-            else if (Input.GetMouseButtonUp(0) && isAiming)
-            {
-                isAiming = false;
-                LaunchBall();
-            }
-
-            UpdateAimLine();
-        }
-    }
-
-    void UpdateAimLine()
-    {
-        lr.enabled = true;
-        lr.SetPosition(0, transform.position);
-        lr.SetPosition(1, transform.position + (Vector3)aimDirection * 5f); // Set the length of the aim line
-    }
-
-    void LaunchBall()
-    {
-        rb.velocity = aimDirection * launchSpeed;
-        isBallStationary = false;
-        lr.enabled = false;
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            rb.velocity = Vector2.zero;
-            rb.angularVelocity = 0f;
-            isBallStationary = true;
-        }    
-    }*/
-
-
-
-
-
-
-
-
-
-
-
-
-    /*OLD MOVEMENT
-     * Rigidbody2D rb;
-
-    float startSpeed = 7.5f;
-    float extraSpeed = 1.5f;
-    float maxSpeed = 4f;
-    int hitCounter = 0;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        StartCoroutine(KickOff(1f));    
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        ContactPoint2D contact = collision.contacts[0];
-
-        if (collision.gameObject.name == "Player")
-        {
-            Bounce(collision);
-        }
-    }
-
-    public IEnumerator KickOff(float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        MoveBall(Vector2.down);
-    }
-
-    void MoveBall(Vector2 direction)
-    {
-        direction = direction.normalized;
-        float ballSpeed = startSpeed + hitCounter * extraSpeed;
-        rb.velocity = direction * ballSpeed;
-    }
-
-    void Bounce(Collision2D collision)
-    {
-        Vector3 ballPosition = transform.position;
-        Vector3 paddlePosition = collision.transform.position;
-        float paddleHeight = collision.collider.bounds.size.y;
-
-        float positionX = (ballPosition.x - paddlePosition.x) / paddleHeight;
-
-        float positionY = 0f;
-        if (collision.gameObject.name == "Player")
-        {
-            positionY = 1;
-        }
-
-        IncreaseHitCounter();
-        MoveBall(new Vector2(positionX, positionY));
-    }
-
-    public void IncreaseHitCounter()
-    {
-        if (hitCounter * extraSpeed < maxSpeed)
-        {
-            hitCounter++;
-        }
-    }*/
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,31 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Ball ball;
+
     float minX = -2.26f;
     float maxX = 2.26f;
+    bool allowMovement;
 
-<<<<<<< HEAD
     void Awake()
     {
-        ball = FindObjectOfType<Ball>(); 
+        ball = FindObjectOfType<Ball>();
+    }
+
+    void Start()
+    {
+        if (!ball.ballIsStationary) { allowMovement = true; }
+        else { allowMovement = false; }
+
     }
 
     void Update()
     {
+        if (!allowMovement)
+            return;
+
         if (ball.ballIsStationary)
-=======
-    void Update()
-    {
-        if (Input.touchCount > 0)
->>>>>>> parent of 20c7fab (Ball & Paddle Working, Just Need to Fix to Frame-Rate for Consistency)
         {
-            Touch touch = Input.GetTouch(0);
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-            MovePaddle(touchPosition);
+            return;
         }
-        else if (Input.GetMouseButton(0))
+        else
         {
-<<<<<<< HEAD
             if (Input.touchCount > 0)
             {
                 Touch touch = Input.GetTouch(0);
@@ -41,28 +45,18 @@ public class PlayerMovement : MonoBehaviour
                 Vector3 touchPosition = Camera.main.ScreenToWorldPoint(inputPosition);
                 MovePaddle(touchPosition);
             }
-<<<<<<< HEAD
-=======
-            Vector3 inputPosition = Input.mousePosition;
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(inputPosition);
-            MovePaddle(touchPosition);
         }
+    }
 
-        void MovePaddle(Vector3 touchPosition)
-        {
-            float clampedX = Mathf.Clamp(touchPosition.x, minX, maxX);
-            float clampedY = Mathf.Clamp(touchPosition.y, transform.position.y, transform.position.y);
-            transform.position = new Vector3(clampedX, clampedY, transform.position.z);
->>>>>>> parent of 20c7fab (Ball & Paddle Working, Just Need to Fix to Frame-Rate for Consistency)
-=======
+    void MovePaddle(Vector3 touchPosition)
+    {
+        float clampedX = Mathf.Clamp(touchPosition.x, minX, maxX);
+        float clampedY = Mathf.Clamp(touchPosition.y, transform.position.y, transform.position.y);
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
+    }
 
-            void MovePaddle(Vector3 touchPosition)
-            {
-                float clampedX = Mathf.Clamp(touchPosition.x, minX, maxX);
-                float clampedY = Mathf.Clamp(touchPosition.y, transform.position.y, transform.position.y);
-                transform.position = new Vector3(clampedX, clampedY, transform.position.z);
-            }
->>>>>>> parent of cc8c119 (ignore)
-        }
+    public void EnableMovement()
+    {
+        allowMovement = true;
     }
 }
